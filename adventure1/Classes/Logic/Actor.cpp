02a49f92af2eb1d2f45ad4af93c24b01217eb4ef
perdhevi <raditya.perdhevi@gmail.com>
2015-04-20 04:06:04 +0700

    fixing folder path

--- adventure1/Classes/Logic/Actor.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Actor.cpp
-//  adventure1
-//
-//  Created by Raditya Perdhevi on 8/11/13.
-//
-//
-
-#include "Actor.h"
--- adventure1/Classes/Logic/BattleActor.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  BattleActor.cpp
-//  adventure1
-//
-//  Created by Raditya Perdhevi on 8/27/13.
-//
-//
-
-#include "BattleActor.h"
